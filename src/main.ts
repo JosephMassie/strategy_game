7b752d339d8c1508758e769704b5ca25fc6065dc
Joseph Massie <josephmmassie@gmail.com>
2025-05-28 17:24:31 -0700

    moved all of main into an async iife to keep the compile target the same and retain the current await setup

--- src/main.ts
+++ src/main.ts
@@ -25,109 +25,110 @@ import TextBox from './components/ui_textbox';
 import Hud from './libraries/hud';
 import Button from './components/ui_button';
 
-const canvas = document.querySelector(
-    'canvas#background'
-)! as HTMLCanvasElement;
+(async () => {
+    const canvas = document.querySelector(
+        'canvas#background'
+    )! as HTMLCanvasElement;
 
-const start = performance.mark('game load');
+    const start = performance.mark('game load');
 
-const loadingContainer = document.createElement('div');
-loadingContainer.classList.add('loading_container');
-loadingContainer.innerHTML = `<div class="loading_text">loading...</div>`;
-const loadingWheel = document.createElement('div');
-loadingWheel.classList.add('loading_wheel');
-loadingContainer.prepend(loadingWheel);
-canvas.insertAdjacentElement('afterend', loadingContainer);
+    const loadingContainer = document.createElement('div');
+    loadingContainer.classList.add('loading_container');
+    loadingContainer.innerHTML = `<div class="loading_text">loading...</div>`;
+    const loadingWheel = document.createElement('div');
+    loadingWheel.classList.add('loading_wheel');
+    loadingContainer.prepend(loadingWheel);
+    canvas.insertAdjacentElement('afterend', loadingContainer);
 
-input.initialize(canvas);
+    input.initialize(canvas);
 
-const engine = getGameEngine(canvas, {
-    autoResize: true,
-    debug: true,
-    useShaders: false,
-});
-
-// preload all the meshes
-const loadingResources: Array<Promise<any>> = [
-    ...TILE_MESHES,
-    ...BUILDING_MESHES,
-    ...OTHER_MESHES,
-].map((path) => loadMesh(addFileExtension('gltf')(path)));
-
-loadingResources.push(
-    new Promise((resolve, reject) => {
-        preloadFont(
-            {
-                font: PIXEL_FONT,
-                characters: '1234567890abcdefghijklmnopqrstuvwxyz',
-                sdfGlyphSize: 16,
-            },
-            () => {
-                console.log(`finished preloading font ${PIXEL_FONT}`);
-                resolve('font loaded');
-            }
-        );
-
-        setTimeout(() => {
-            reject(`failed to preload font`);
-        }, 1500);
-    })
-);
-
-await Promise.all(loadingResources)
-    .then(() => {
-        console.log(`finished loading all resources`);
-    })
-    .catch((reason) => {
-        console.error(`a resource failed to load: ${reason}`);
+    const engine = getGameEngine(canvas, {
+        autoResize: true,
+        debug: true,
+        useShaders: false,
     });
 
-const scene = engine.createScene();
-engine.setActiveScene(scene);
+    // preload all the meshes
+    const loadingResources: Array<Promise<any>> = [
+        ...TILE_MESHES,
+        ...BUILDING_MESHES,
+        ...OTHER_MESHES,
+    ].map((path) => loadMesh(addFileExtension('gltf')(path)));
 
-engine.setActiveHudScene(engine.createScene());
+    loadingResources.push(
+        new Promise((resolve, reject) => {
+            preloadFont(
+                {
+                    font: PIXEL_FONT,
+                    characters: '1234567890abcdefghijklmnopqrstuvwxyz',
+                    sdfGlyphSize: 16,
+                },
+                () => {
+                    console.log(`finished preloading font ${PIXEL_FONT}`);
+                    resolve('font loaded');
+                }
+            );
 
-Hud.initialize();
+            setTimeout(() => {
+                reject(`failed to preload font`);
+            }, 1500);
+        })
+    );
 
-const ambientLight = new T.AmbientLight(0xffffff, 1);
-scene.add(ambientLight);
+    await Promise.all(loadingResources)
+        .then(() => {
+            console.log(`finished loading all resources`);
+        })
+        .catch((reason) => {
+            console.error(`a resource failed to load: ${reason}`);
+        });
 
-const sun = new T.DirectionalLight(0xffffff, 3);
-sun.position.set(...SUN_POS);
-scene.add(sun);
+    const scene = engine.createScene();
+    engine.setActiveScene(scene);
 
-engine.createToonShader();
+    engine.setActiveHudScene(engine.createScene());
 
-const rows = 100;
-const columns = rows;
-const tileSize = 12;
-const width = rows * tileSize;
+    Hud.initialize();
 
-const map = new LvlMap(
-    rows,
-    columns,
-    new T.Vector3(-width / 2, -3, -width / 2)
-);
-map.addToScene();
+    const ambientLight = new T.AmbientLight(0xffffff, 1);
+    scene.add(ambientLight);
 
-canvas.focus();
+    const sun = new T.DirectionalLight(0xffffff, 3);
+    sun.position.set(...SUN_POS);
+    scene.add(sun);
 
-const resourceMonitor = new TextBox(
-    `Minerals: ${getResource(ResourceTypes.MINERALS)}\nFood: ${getResource(
-        ResourceTypes.FOOD
-    )}`,
-    new T.Vector2(-0.98, 0.98),
-    {
-        anchor: 'top left',
-        backDropColor: 0x1f1f1f,
-        letterSpacing: 0.05,
-    }
-);
-resourceMonitor.addToHudScene();
+    engine.createToonShader();
 
-// controls callout
-const ctrlCallout = new TextBox(
-    `Controls:
+    const rows = 100;
+    const columns = rows;
+    const tileSize = 12;
+    const width = rows * tileSize;
+
+    const map = new LvlMap(
+        rows,
+        columns,
+        new T.Vector3(-width / 2, -3, -width / 2)
+    );
+    map.addToScene();
+
+    canvas.focus();
+
+    const resourceMonitor = new TextBox(
+        `Minerals: ${getResource(ResourceTypes.MINERALS)}\nFood: ${getResource(
+            ResourceTypes.FOOD
+        )}`,
+        new T.Vector2(-0.98, 0.98),
+        {
+            anchor: 'top left',
+            backDropColor: 0x1f1f1f,
+            letterSpacing: 0.05,
+        }
+    );
+    resourceMonitor.addToHudScene();
+
+    // controls callout
+    const ctrlCallout = new TextBox(
+        `Controls:
 Click tiles to build
     WASD to move
     QE and Arrow Keys to rotate
@@ -141,102 +142,108 @@ Build MINES on mountains and FARMS on grass
     MINES costs 20 minerals
     FARMS cost 5 minerals
     MINES also have an upkeep cost of 5 food`,
-    new T.Vector2(-0.98, -0.98),
-    { backDropColor: 0x1f1f1f, letterSpacing: 0.05 }
-);
-ctrlCallout.addToHudScene();
+        new T.Vector2(-0.98, -0.98),
+        { backDropColor: 0x1f1f1f, letterSpacing: 0.05 }
+    );
+    ctrlCallout.addToHudScene();
 
-let buildModeOn = false;
-const buildBtn = new Button({
-    width: 200,
-    height: 50,
-    relativeScreenPos: new T.Vector2(0.1, -0.9),
-    text: 'Toggle Build',
-});
-buildBtn.onClick = (mbutton) => {
-    if (mbutton === MouseButton.LEFT) {
-        buildModeOn = !buildModeOn;
-    }
-};
-
-let buildings: Array<Building> = [];
-
-let isRunning = true;
-let lastTimeStamp: number = performance.now();
-
-function gameLoop(now: number) {
-    if (!isRunning) return;
-
-    if (lastTimeStamp === undefined) lastTimeStamp = now;
-
-    const deltaTime = now - lastTimeStamp;
-    lastTimeStamp = now;
-
-    updateTimers(deltaTime);
-    input.update();
-
-    const hudInteracted = Hud.update();
-
-    if (input.isKeyPressed('Escape')) {
-        console.log('Escape pressed, closing game');
-        isRunning = false;
-    }
-    engine.update();
-    map.update();
-
-    buildings.forEach((building) => {
-        building.update();
+    let buildModeOn = false;
+    const buildBtn = new Button({
+        width: 200,
+        height: 50,
+        relativeScreenPos: new T.Vector2(0.1, -0.9),
+        text: 'Toggle Build',
     });
+    buildBtn.onClick = (mbutton) => {
+        if (mbutton === MouseButton.LEFT) {
+            buildModeOn = !buildModeOn;
+        }
+    };
 
-    if (
-        !hudInteracted &&
-        buildModeOn &&
-        input.isMouseButtonPressed(MouseButton.LEFT)
-    ) {
-        const focusedTile = map.getHoveredTile();
-        if (focusedTile !== null) {
-            switch (focusedTile.terrain) {
-                case Terrain.MOUNTAIN:
-                    if (mineConstructor.checkCost()) {
-                        const mine = mineConstructor.build(
-                            focusedTile.position
-                        );
-                        mine.addToScene(scene);
-                        buildings.push(mine);
-                    }
-                    break;
-                case Terrain.GRASS:
-                    if (farmConstructor.checkCost()) {
-                        const farm = farmConstructor.build(
-                            focusedTile.position
-                        );
-                        farm.addToScene(scene);
-                        buildings.push(farm);
-                    }
-                    break;
-                case Terrain.WATER:
-                    break;
-                case Terrain.SAND:
-                    break;
-                default:
+    let buildings: Array<Building> = [];
+
+    let isRunning = true;
+    let lastTimeStamp: number = performance.now();
+
+    function gameLoop(now: number) {
+        if (!isRunning) return;
+
+        if (lastTimeStamp === undefined) lastTimeStamp = now;
+
+        const deltaTime = now - lastTimeStamp;
+        lastTimeStamp = now;
+
+        updateTimers(deltaTime);
+        input.update();
+
+        const hudInteracted = Hud.update();
+
+        if (input.isKeyPressed('Escape')) {
+            console.log('Escape pressed, closing game');
+            isRunning = false;
+        }
+        engine.update();
+        map.update();
+
+        buildings.forEach((building) => {
+            building.update();
+        });
+
+        if (
+            !hudInteracted &&
+            buildModeOn &&
+            input.isMouseButtonPressed(MouseButton.LEFT)
+        ) {
+            const focusedTile = map.getHoveredTile();
+            if (focusedTile !== null) {
+                switch (focusedTile.terrain) {
+                    case Terrain.MOUNTAIN:
+                        if (mineConstructor.checkCost()) {
+                            const mine = mineConstructor.build(
+                                focusedTile.position
+                            );
+                            mine.addToScene(scene);
+                            buildings.push(mine);
+                        }
+                        break;
+                    case Terrain.GRASS:
+                        if (farmConstructor.checkCost()) {
+                            const farm = farmConstructor.build(
+                                focusedTile.position
+                            );
+                            farm.addToScene(scene);
+                            buildings.push(farm);
+                        }
+                        break;
+                    case Terrain.WATER:
+                        break;
+                    case Terrain.SAND:
+                        break;
+                    default:
+                }
             }
         }
+
+        const curMinerals = getResource(ResourceTypes.MINERALS);
+        const curFood = getResource(ResourceTypes.FOOD);
+        resourceMonitor.updateText(
+            `Minerals: ${curMinerals}\nFood: ${curFood}`
+        );
+
+        engine.render(deltaTime);
+
+        requestAnimationFrame(gameLoop);
     }
 
-    const curMinerals = getResource(ResourceTypes.MINERALS);
-    const curFood = getResource(ResourceTypes.FOOD);
-    resourceMonitor.updateText(`Minerals: ${curMinerals}\nFood: ${curFood}`);
+    // Wait to start the game loop after all primary resources are loaded
+    const startGame = () => {
+        console.log(
+            `starting game`,
+            performance.measure('game load', start.name)
+        );
+        loadingContainer.style.display = 'none';
+        gameLoop(lastTimeStamp);
+    };
 
-    engine.render(deltaTime);
-
-    requestAnimationFrame(gameLoop);
-}
-
-// Wait to start the game loop after all primary resources are loaded
-const startGame = () => {
-    console.log(`starting game`, performance.measure('game load', start.name));
-    loadingContainer.style.display = 'none';
-    gameLoop(lastTimeStamp);
-};
-
-startGame();
+    startGame();
+})();
